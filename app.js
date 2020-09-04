3d4b49efe0e530b751617be08d46edd85549d9f6
securitytoucan <you@example.com>
2020-09-04 12:03:59 +0400

    added a landing page

--- app.js
+++ app.js
@@ -34,5 +34,7 @@ app.use(adminRoutes);
 //     console.log("Server has started on port 3000")
 // })
 app.listen(process.env.PORT, process.env.IP, ()=> {
-    console.log("Server has started on port 3000")
+    app.get("/", (req,res) => {
+        res.render("landing.ejs")
+    });
 })
